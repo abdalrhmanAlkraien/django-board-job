b49b3658935eee00defe6f5c7eecd31819b1df8d
Abd-Alrhman Alkraien <Abdalrhmanalkraien@gmail.com>
2020-10-12 11:29:32 +0300

    add job model on the database

--- Job/models.py
+++ Job/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+JOB_TYPE = (
+            ('Full Time',"Full Time"),
+            ("Part Time","Part Time")
+          )
+class job (models.Model):
+    title=models.CharField(max_length=50)
+    #location
+    job_type=models.CharField(max_length=15,choices=JOB_TYPE)
+    decription=models.TextField(max_length=500,default=None,null=True)
+    published_at=models.DateTimeField(auto_now=True)
+    vacancy=models.IntegerField(default=1)
+    salary=models.IntegerField(default=0)
+    experience=models.IntegerField(default=0)
+    def __str__(self):
+        return self.title
+    
--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #my Apps
+    'Account',
+    'Job',
+    'Home',
+    'Blog',
+    'Contact'
+    
+
 ]
 
 MIDDLEWARE = [
