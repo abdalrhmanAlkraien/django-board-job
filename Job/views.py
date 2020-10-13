b8572814ec1cade25379d170dbdcc7aec657b87d
Abd-Alrhman Alkraien <Abdalrhmanalkraien@gmail.com>
2020-10-13 22:50:06 +0300

    add page ination

--- Job/views.py
+++ Job/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render,get_object_or_404
 from .models import job
+from django.core.paginator import Paginator
 
 def job_list(request):
     jobs=job.objects.all()
-    context={'jobs':jobs}
+    paginator = Paginator(jobs, 1) 
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context={'jobs':page_obj}
     return render(request,'job\job_list.html',context)
 
 def job_details(request,id):
