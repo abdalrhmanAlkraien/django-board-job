72d89e2f563a515ffc2c4bdadc0c83be5aaa07b7
Abd-Alrhman Alkraien <Abdalrhmanalkraien@gmail.com>
2020-10-15 17:13:28 +0300

    add profile view task completed

--- Account/urls.py
+++ Account/urls.py
@@ -4,8 +4,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 
+
 app_name='accounts'
 urlpatterns = [
     path('signup',views.signup,name="signup"),
+    path('profile',views.profile_view,name='profile'),
+    path('edit_profile',views.edit_profile,name='edit_profile')
     
 ]
--- Account/views.py
+++ Account/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
-from .forms import SignUpForm
+from .forms import SignUpForm 
 from django.contrib.auth import authenticate,login
+from .models import profile
+from django.contrib.auth.models import User
 
 # Create your views here.
 def signup(request):
@@ -19,3 +21,17 @@ def signup(request):
         form=SignUpForm()
     context={'form':form}
     return render(request,'registration/signup.html',context)
+
+def profile_view(request):
+    if request.user.is_authenticated:
+        #if Profile.objects.filter(user=request.user).exists():
+         prof=profile.objects.get(user=request.user)
+         return render(request,'accounts/profile.html',{'profile':prof})
+    else:
+        return redirect('login')
+    
+
+def edit_profile(request):
+    pass
+
+
