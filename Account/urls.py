8931eb240f7d8a9621ccffb8b83c4a9dbb547f26
Abd-Alrhman Alkraien <Abdalrhmanalkraien@gmail.com>
2020-10-15 03:41:36 +0300

    Add signup task

--- Account/urls.py
+++ Account/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 
+app_name='accounts'
 urlpatterns = [
     path('signup',views.signup,name="signup"),
     
--- project/urls.py
+++ project/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('accounts/',include('django.contrib.auth.urls')),
+    path('accounts/',include('Account.urls',namespace="accounts")),
     path('admin/', admin.site.urls),
     path('job/',include('Job.urls' ,namespace="Job")),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)#+static(settings.MEDIA_URL,document_root=settings.MEDIA_URL)
